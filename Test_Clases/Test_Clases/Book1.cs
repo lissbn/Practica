77806f71f0afe2af06300533c496da8b3d8dfaf8
Lissa Bueno <lissabn9@gmail.com>
2025-04-09 11:49:12 -0400

    Practica de creacion de clases 2.

---
+++ Test_Clases/Test_Clases/Book1.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Book
+{
+    public string title; // creacion del elemento publico de la clase title
+    public string author; // creacion del elemento publico de la clase author
+    public int pages; // creacion del elemento publico de la clase pages
+
+    /*
+    public Book()
+    {
+
+        Console.WriteLine("Creating Book class");
+    }
+    */
+}
---
+++ Test_Clases/Test_Clases/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+        Book book1 = new Book(); // aqui se crea un elemento "book1" de tipo Book
+        book1.title = "Harry";   // Asi se asigna el title al book1
+        book1.author = "JK";     // asignacion del nombre del autor
+        book1.pages = 400;       // numero de paginas del libro
+
+        Book book2 = new Book(); //Aqui se crea un book2 de tipo Book
+        book2.title = "Rings";
+        book2.author = "Tolkien";
+        book2.pages = 700;
+
+
+        Console.WriteLine(book1.title); // Muestra en pantalla el titulo del libro 1
