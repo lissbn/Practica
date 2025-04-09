df9421d4831d9d6ae127476c7b9a6ce703b0c733
Lissa Bueno <lissabn9@gmail.com>
2025-04-09 13:28:22 -0400

    Prueba de clases

---
+++ Test_Clases/Test_Clases/Book.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Book // internal class ... solamente pueden acceder a la clase los archivos de la carpeta
+// private trabaja solo dentro de la clase
+// public funciona para acceder desde cualquier archivo donde hay una instancia de la clase
+// static para acceder a las variables sin instancear  inicializar la clase
+{
+    public string title; // creacion del elemento público de la clase title
+    public string author; // creacion del elemento público de la clase author
+    public int pages; // creacion del elemento público de la clase pages
+
+    public Book() // Este es un constructor. Puede recibir parametros
+    {
+    }// inicializar constructor sin parametros.
+        
+    public Book(string aTitle, string aAuthor, int aPages) // Este es un constructor. Puede recibir parametros
+    {
+        title = aTitle; // asignando los parametros a las variables
+        author = aAuthor;
+        pages = aPages;
+       // Console.WriteLine(name);
+    }
+    
+}
--- Test_Clases/Test_Clases/Program.cs
+++ Test_Clases/Test_Clases/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
+/*
+ // manera de crear book1 con parametros
         Book book1 = new Book(); // aqui se crea un elemento "book1" de tipo Book
         book1.title = "Harry";   // Asi se asigna el title al book1
         book1.author = "JK";     // asignacion del nombre del autor
@@ -11,6 +12,25 @@ Console.WriteLine("Hello, World!");
         book2.title = "Rings";
         book2.author = "Tolkien";
         book2.pages = 700;
+*/
 
+// Manera mas eficiente de dar parametros a la clase
 
-        Console.WriteLine(book1.title); // Muestra en pantalla el titulo del libro 1
+Book book8 = new Book();
+
+book8.title = "test";
+
+        Book book3 = new Book("Harry Potter", "JK Rowling", 400);
+        Book book4 = new Book("Lord of the Rings", "Tolkien", 700);
+
+        book3.title = "Hobbit"; // de esta manera se puede cambiar la informacion dentro de la variable
+
+        Console.WriteLine(book8.title); // Muestra en pantalla el titulo del libro 1
+
+/*
+ // Recibir una entrada tecleada por el usuario y mostrarla en pantalla
+        string entrada = Console.ReadLine();
+        string entradaNumerica = Console.ReadLine();
+        Console.WriteLine(entradaNumerica);
+*/    
+        
